4035c080681c63e1bace02c452e1b347537198a9
isybliss <isyblissumunna@gmail.com>
2022-10-17 09:57:09 +0100

    printf

--- main.h
+++ main.h
@@ -7,6 +7,20 @@
 #include <limits.h>
 #include <unistd.h>
 
+#define UNSED(x) (void)(x)
+#define BUFF_SIZE 1024
+
+/* FLAGS */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+/* SIZES */
+#define S_LONG 2
+#define S_SHORT 1
+
 
 
 /**
@@ -18,28 +32,78 @@
 
 typedef struct format
 {
-	char *id;
-	int (*f)();
-} convert_match;
+	char format;
+	int (*f)(va_list, char[], int, int, int, int);
+} fmt_t;
 
-int printf_pointer(va_list val);
+int _printf(const char *format, ...);
+int handle_print(const char *fmt, int *i,
+va_list list, char buffer[], int flags, int width, int precision, int size);
+
+/***** FUNCTIONS ******/
+
+int printf_char(va_list val, char buffer[],
+	int flags, int width, int precision, int size);
+int printf_string(va_list val, char buffer[],
+	int flags, int width, int precision, int size);
+int printf_percent(va_list val, char buffer[],
+	int flags, int width, int precision, int size);
+int printf_int(va_list val, char buffer[],
+	int flags, int width, int precision, int size);
+int printf_binary(va_list val, char buffer[],
+	int flags, int width, int precision, int size);
+int printf_unsigned(va_list val, char buffer[],
+	int flags, int width, int precision, int size);
+int printf_octal(va_list val, char buffer[],
+	int flags, int width, int precision, int size);
+int printf_hexadecimal(va_list val, char buffer[],
+	int flags, int width, int precision, int size);
+int printf_hexa_upper(va_list val, char buffer[],
+	int flags, int width, int precision, int size);
+
+int printf_hexa(va_list val, char map_to[],
+char buffer[], int flags, char flag_ch, int width, int precision, int size);
+
+int printf_non_printable(va_list val, char buffer[],
+	int flags, int width, int precision, int size);
+
+int printf_pointer(va_list val, char buffer[],
+		int flags, int width, int precision, int size);
+
+/* Functions to handle other specifiers */
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
+
+int printf_reverse(va_list val, char buffer[],
+	int flags, int width, int precision, int size);
+
+
+int printf_rot13string(va_list val, char buffer[],
+	int flags, int width, int precision, int size);
 int printf_hex_aux(unsigned long int num);
 int printf_HEX_aux(unsigned int num);
-int printf_exclusive_string(va_list val);
-int printf_HEX(va_list val);
-int printf_hex(va_list val);
-int printf_oct(va_list val);
-int printf_unsigned(va_list args);
-int printf_bin(va_list val);
-int printf_srev(va_list args);
-int printf_rot13(va_list args);
-int printf_int(va_list args);
-int printf_dec(va_list args);
-int _strlen(char *s);
-int *_strcpy(char *dest, char *src);
-int _strlenc(const char *s);
-int rev_string(char *s);
-int _strlenc(const char *s);
-int printf_percent(void);
+
+/* width handler */
+int handle_write_char(char c, char buffer[],
+	int flags, int width, int precision, int size);
+int write_number(int is_positive, int ind, char buffer[],
+	int flags, int width, int precision, int size);
+int write_num(int ind, char bff[], int flags, int width, int precision,
+	int length, char padd, char extra_c);
+int write_pointer(char buffer[], int ind, int length,
+	int width, int flags, char padd, char extra_c, int padd_start);
+
+int write_unsgnd(int is_negative, int ind, char buffer[],
+		int flags, int width, int precision, int size);
+
+/****************** UTILS ******************/
+int is_printable(char);
+int append_hexa_code(char, char[], int);
+int is_digit(char);
+
+long int convert_size_number(long int num, int size);
+long int convert_size_unsgnd(unsigned long int num, int size);
 
 #endif
